ac7ba578d0993552f2606afde6fde237622902b8
gusttavosant <gusttavosant@gmailcom>
2021-12-08 21:43:20 -0300

    filtro da pokedex

--- src/app/pages/pokedex/pokedex.page.ts
+++ src/app/pages/pokedex/pokedex.page.ts
@@ -7,14 +7,33 @@ import { PokemonService } from 'src/app/services/pokemon/pokemon.service';
   templateUrl: 'pokedex.page.html',
   styleUrls: ['pokedex.page.scss']
 })
+
+
 export class PokedexPage {
 
   lsPokemon; 
+  lsPokemonTotal;
+  nomePokemon:string;
   constructor(private serviceCadastro: CadastroService, private pokemon: PokemonService) {
+
+  }
+
+  filtrar(){
+    // console.log(this.nomePokemon);
+    // return
+    console.log(this.lsPokemonTotal);
+    
+    if(!this.nomePokemon){
+      this.lsPokemon = [...this.lsPokemonTotal] 
+    } else {
+    const pokemonFiltrado = this.lsPokemonTotal.filter(pokemon => pokemon.name.includes(this.nomePokemon));
+    this.lsPokemon = [...pokemonFiltrado]
+    }
   }
 
   ngOnInit(){
     this.serviceCadastro.existeLogin();
-    this.lsPokemon = this.pokemon.listaPokemon(); 
+    this.lsPokemon = this.pokemon.listaPokemon();
+    this.lsPokemonTotal = this.pokemon.listaPokemon();
   }
 }
